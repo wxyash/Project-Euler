fbb4fe73cbb42377e33dd04715096592e02131ed
Yash Thanki <yash@unoapp.com>
2019-10-23 14:07:40 -0400

    q-1, done

---
+++ q-1.py
@@ -0,0 +1,7 @@
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+# _sum = sum([num for num in range(10) if ((num%3 ==0) or (num%5 ==0) ) ]) # == 23
+_sum = sum([num for num in range(1000) if ((num%3 ==0) or (num%5 ==0) ) ]) # == 233168
+
+
